8e3da0339c6168d402ac392c52d675cedb52243e
qinghua <2469972952@qq.com>
2021-11-29 08:48:05 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
 import './assets/css/icon.css'
+import print from 'vue3-print-nb'
+import 'font-awesome/css/font-awesome.min.css';
 const app = createApp(App)
 installElementPlus(app)
 app
     .use(store)
     .use(router)
+    .use(print)
     .mount('#app')
